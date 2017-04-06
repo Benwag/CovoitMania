10e85e5ab33cd72eae5fbb344b1af43072707f77
JeremieWenger <jeremie.wenger@outlook.com>
2017-04-06 17:51:51 +0200

    Methode de calcul distance

--- src/com/cfranc/UserManger/model/CalculDistance.java
+++ src/com/cfranc/UserManger/model/CalculDistance.java
@@ -8,5 +8,5 @@ public class CalculDistance {
 		double resultat=RADIUS*Math.acos(Math.sin(latA*2*Math.PI/360)*Math.sin(latB*2*Math.PI/360)+Math.cos(latA*2*Math.PI/360)*Math.cos(latB*2*Math.PI/360)*Math.cos(longA*2*Math.PI/360-longB*2*Math.PI/360));
 		return resultat;
 	}
-
+ 
 }
